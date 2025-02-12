556c8c58c23e29f5ec8351a4e20eda4064addb6c
bohdan <shovkopliasbohdan@gmail.com>
2025-02-12 16:48:18 +0200

    Rehaul: CommentsSection design

--- src/pages/QuestPage/Comment.tsx
+++ src/pages/QuestPage/Comment.tsx
@@ -1,56 +1,58 @@
-import { Avatar, Button, Dialog, TextArea } from '@radix-ui/themes';
 import {
-  IComment,
-  useDeleteCommentByIdMutation
-} from '../../store/comment/comment.api';
-import { useAppSelector } from '../../store/store';
-import Modal from '../../components/ui/Modal';
+   Avatar,
+   Button,
+   Card,
+   Dialog,
+   TextArea,
+   Tooltip
+} from "@radix-ui/themes";
+import {
+   IComment,
+   useDeleteCommentByIdMutation
+} from "../../store/comment/comment.api";
+import { useAppSelector } from "../../store/store";
+import Modal from "../../components/ui/Modal";
 
 interface Props {
-  comment: IComment;
+   comment: IComment;
 }
 
 const Comment = ({ comment }: Props) => {
-  const { user } = useAppSelector((store) => store.auth);
+   const { user } = useAppSelector((store) => store.auth);
 
-  const [removeComment, { isLoading: isCommentDeleting }] =
-    useDeleteCommentByIdMutation();
+   const [removeComment, { isLoading: isCommentDeleting }] =
+      useDeleteCommentByIdMutation();
 
-  const handleRemoveComment = (commentId: number) => () => {
-    if (!isCommentDeleting)
-      removeComment(commentId).unwrap().catch(console.log);
-  };
+   const handleRemoveComment = (commentId: number) => () => {
+      if (!isCommentDeleting)
+         removeComment(commentId).unwrap().catch(console.log);
+   };
 
-  return (
-    <div key={comment.id} className="flex gap-2 items-stretch">
-      <Avatar fallback="A" src={comment.userDTO.image} size="3" />
-      <TextArea disabled value={comment.title} className="flex-grow" />
-      {user && comment.userDTO.id === user.id && (
-        <>
-          <Modal
-            trigger={<Button>Delete comment</Button>}
+   return (
+      <div className="flex gap-2 items-stretch">
+         <Tooltip
             content={
-              <>
-                <Dialog.Title className="text-center">
-                  Are you sure you want to remove this comment?
-                </Dialog.Title>
-                <div className="flex gap-2 justify-center">
-                  <Dialog.Close>
-                    <Button variant="soft" color="gray">
-                      Cancel
-                    </Button>
-                  </Dialog.Close>
-                  <Button color="red" onClick={handleRemoveComment(comment.id)}>
-                    Remove <p className="pi pi-sign-out"></p>
-                  </Button>
-                </div>
-              </>
+               <>
+                  <p>User id: {comment.userDTO.id}</p>
+                  {user && comment.userDTO.id === user.id && (
+                     <Button
+                        color="red"
+                        onClick={handleRemoveComment(comment.id)}
+                        className="!mt-1"
+                     >
+                        Delete comment<p className="pi pi-trash"></p>
+                     </Button>
+                  )}
+               </>
             }
-          />
-        </>
-      )}
-    </div>
-  );
+         >
+            <div>
+               <Avatar fallback="A" src={comment.userDTO.image} size="2" />
+            </div>
+         </Tooltip>
+         <p className="flex-grow">{comment.title}</p>
+      </div>
+   );
 };
 
 export default Comment;
